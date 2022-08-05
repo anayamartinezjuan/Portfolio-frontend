70aedff0d088cab1c875a272c17fdf212e0b5082
Juan Anaya <juanestebananayamartinez@gmail.com>
2022-08-04 20:15:48 -0500

    se agregaron las secciones de abouty portfolio

---
+++ src/Components/About/About.js
@@ -0,0 +1,33 @@
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+import './About.css'
+
+function About(){
+    const [information, setInformation] = useState({});
+    useEffect(() => {
+        axios.get('https://backend-porfolio-juan.herokuapp.com/about')
+        .then(
+            res => setInformation(res.data[0])
+        )}, [setInformation])
+    
+    return(
+        <div className='About'>
+            <div className='div-foto'>
+                <img className='foto-perfil' alt='FotoPerfil' src={information.foto}></img>
+            </div>
+            <div className='Descripcion'>
+                <h1>{information.nombre}</h1>
+                <h2>{information.profesión}</h2>
+                <p>{information.about_me}</p>
+                <ul className='skills'>
+                    {information.habilidades && information.habilidades.map((habilidad, index) => 
+                        <p className='habilidad' key={index}>{habilidad}</p> 
+                        
+                    )}
+                </ul>
+            </div>
+        </div>
+    )
+}; 
+
+export default About;
---
+++ src/Components/Portfolio/Portfolio.js
@@ -0,0 +1,30 @@
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+import './Portfolio.css'
+
+function Portfolio(){
+    const [projects, setProjects] = useState({});
+    useEffect(() => {
+        axios.get('https://backend-porfolio-juan.herokuapp.com/portfolio')
+        .then(
+            res => setProjects(res.data)
+    )}, [setProjects])
+    return(
+        <>
+            <div className='mega-card-portfolio'>
+                { projects.length > 0 && projects.map((project, index) => 
+                   
+                   <div className='individual-card' key={index}>
+                       <h1>{project.nombre}</h1>
+                       <img className='imagen-proyecto' alt='imagen-proyecto' src={project.imagen}></img> 
+                       <p>{project.descripcion}</p>
+                       <a className='boton-visitar' href={project.link}>Visitar Proyecto</a>
+                   </div> 
+                )}
+            </div>
+            
+        </>
+    )
+}; 
+
+export default Portfolio;
